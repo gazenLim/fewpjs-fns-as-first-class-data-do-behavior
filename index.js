5592165ef02927cdda43c53ab810056f32a7397b
gajen <meetgajen@yahoo.com>
2021-06-14 10:47:38 -0400

    commit tree b9fd6c1246a4f48b7cf20b4873d1f1b33f8067ac

--- index.js
+++ index.js
@@ -10,4 +10,31 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(the_time){
+  debugger
+  let this_time = the_time.split(':')
+  let another_time = this_time[0].concat(this_time[1])
+  let the_result = parseInt(another_time, 10)
+
+
+  if(the_result < 12){
+    
+        return "Good Morning";
+  }
+  else if((the_result >= 12) && (the_result<=17)){
+    return "Good Afternoon";
+  }
+
+  else if((the_result > 17 ) && ( the_result <=24)){
+    return "Good Evening";
+  }
+
+else{
+  return "TEST";
+}
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg){
+  document.getElementById("greeting").innerHTML = msg;
+}
